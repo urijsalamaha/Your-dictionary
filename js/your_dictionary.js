fc7119fd2f711e5f7c2e596194d708aa61406369
Yuri <urijsalamaha@gmail.com>
2020-11-05 22:09:21 +0200

    form second option

--- js/your_dictionary.js
+++ js/your_dictionary.js
@@ -2,8 +2,22 @@ var buttonSignUp = document.getElementById('button-sign-up');
 var curtain = document.getElementById('curtain');
 var signUp = document.getElementById('sign-up');
 
+var deleteWindow = function() {
+    curtain.classList.add('display-none');
+    signUp.classList.add('display-none');
+}
+var resetCurtainDelete = function() {
+    curtain.classList.remove('curtain-delete');
+    signUp.classList.remove('signUp-delete');
+}
 
 buttonSignUp.onclick = function() {
     curtain.classList.remove('display-none');
     signUp.classList.remove('display-none');
+}
+curtain.onclick = function(){
+    this.classList.add('curtain-delete');
+    signUp.classList.add('signUp-delete');
+    setTimeout(deleteWindow, 500);
+    setTimeout(resetCurtainDelete, 500);
 }
